af6f9471ca6b50765c1408b07ecd1ced645020c0
Owen Raccuglia <ojracc@gmail.com>
2019-07-16 10:25:22 -0400

    Remove some Python 2.7.3 incompatible options
    We'll be done with this repo soon enough. Until then, delete stuff that blocks installation.

--- setup.py
+++ setup.py
@@ -37,9 +37,4 @@ setup(name='django-registration',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Topic :: Utilities'],
-      python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
-      install_requires=[
-          'Django>=1.11',
-          'confusable_homoglyphs~=3.0',
-      ],
 )
